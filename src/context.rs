74cee238075bf941e99fca45f90cb5f426136243
hus3h <39054982+hus3h@users.noreply.github.com>
2023-07-16 19:49:23 +0300

    Add context constructor and allow empty router

--- src/context.rs
+++ src/context.rs
@@ -1,44 +1,55 @@
 use std::rc::Rc;
 
 pub struct Context {
-    pub router: Option<Router>,
+    pub router: Router,
 }
 
 impl Context {
+    pub fn new() -> Self {
+        Self {
+            router: Router::new(),
+        }
+    }
+
     pub fn route(
         &mut self,
         default_route_path: &str,
         routes: Vec<RouterPath>,
     ) -> Result<(), String> {
-        if self.router.is_none() {
-            let mut final_routes = vec![];
-            let mut default_route = None;
-            for path in routes {
-                let is_default_path = path.path == default_route_path;
-                let new_path = Rc::new(path);
-                if is_default_path {
-                    default_route = Some(Rc::clone(&new_path));
-                }
-                final_routes.push(new_path);
+        let mut final_routes = vec![];
+        let mut default_route = None;
+        for path in routes {
+            let is_default_path = path.path == default_route_path;
+            let new_path = Rc::new(path);
+            if is_default_path {
+                default_route = Some(Rc::clone(&new_path));
             }
-            if let Some(value) = default_route {
-                self.router = Some(Router {
-                    routes: final_routes,
-                    default_route: value,
-                });
-                Ok(())
-            } else {
-                Err("Invalid default route path".to_string())
-            }
+            final_routes.push(new_path);
+        }
+        if let Some(value) = default_route {
+            self.router = Router {
+                routes: final_routes,
+                default_route: Some(value),
+            };
+            Ok(())
         } else {
-            Err("Cannot call this function more than once".to_string())
+            Err("Invalid default route path".to_string())
         }
     }
 }
 
 pub struct Router {
     pub routes: Vec<Rc<RouterPath>>,
-    pub default_route: Rc<RouterPath>,
+    pub default_route: Option<Rc<RouterPath>>,
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Router {
+            routes: Default::default(),
+            default_route: None,
+        }
+    }
 }
 
 pub struct RouterPath {
